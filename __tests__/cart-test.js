8e3b54f1d255ba6b234a3293182df990d01efa27
ywang <657316992@qq.com>
2014-12-25 18:13:30 +0800

    add #getgetCartItemsText() test to  cart-test.js

--- __tests__/cart-test.js
+++ __tests__/cart-test.js
@@ -1,3 +1,4 @@
+jest.dontMock('../src/model/promotion-util');
 jest.dontMock('../src/model/cart');
 jest.dontMock('lodash');
 
@@ -30,4 +31,23 @@ describe('Cart', function() {
       expect(cart.cartItems[0].count).toBe(2);
     })
   });
+  describe('#getCartItemsText()', function() {
+    it('should return cartItemsText', function() {
+
+      var getCartItemsText = jest.genMockFn();
+      var cartItemsText = '名称：雪碧，数量：5瓶，单价：3.00(元)，小计：12.00(元)\n' +
+      '名称：荔枝，数量：2斤，单价：15.00(元)，小计：30.00(元)\n' +
+      '名称：方便面，数量：3袋，单价：4.50(元)，小计：9.00(元)\n';
+      getCartItemsText.mockReturnValue(cartItemsText);
+
+      var Cart ={
+        getCartItemsText : getCartItemsText
+      }
+
+      result = Cart.getCartItemsText(cartItem);
+
+      expect(cartItemsText).toBe(result);
+    })
+  });
+
 });
